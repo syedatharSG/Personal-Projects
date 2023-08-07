7e5db7b61bb79f0e1a6e840d838f2389af89d86d
Syed <syed@shoppinggives.com>
2023-08-07 15:15:44 -0500

    [In progress changes] - Portfolio Components

---
+++ In-Progress/components/Skills.jsx
@@ -0,0 +1,23 @@
+const Skills = () => {
+    return (
+      <div className="bg-gray-100 dark:bg-gray-800">
+        <h2 className="font-general-semibold text-3xl text-gray-800 mb-3">Skills</h2>
+        <div className="flex flex-wrap items-center justify-center">
+          <span className="font-general-medium border border-indigo-200 dark:border-ternary-dark sm:py-3 bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500 px-4 py-2 rounded-lg shadow-lg mx-2 my-1 mr-4">
+            C#
+          </span>
+          <span className="font-general-medium border border-indigo-200 dark:border-ternary-dark sm:py-3 bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500 px-4 py-2 rounded-lg shadow-lg mx-2 my-1 mr-4">
+            .NET
+          </span>
+          <span className="font-general-medium border border-indigo-200 dark:border-ternary-dark sm:py-3 bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500 px-4 py-2 rounded-lg shadow-lg mx-2 my-1 mr-4">
+            JavaScript
+          </span>
+          <span className="font-general-medium border border-indigo-200 dark:border-ternary-dark sm:py-3 bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500 px-4 py-2 rounded-lg shadow-lg mx-2 my-1 mr-4">
+            SQL
+          </span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Skills;
