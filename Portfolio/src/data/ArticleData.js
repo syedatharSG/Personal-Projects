cea5728c713f3af4257eaaa55ba9096edf21e1de
syedathar2 <syedathar319@gmail.com>
2023-10-08 15:21:26 -0500

    Update ArticleData.js

--- Portfolio/src/data/ArticleData.js
+++ Portfolio/src/data/ArticleData.js
@@ -71,14 +71,6 @@ export const articleData = [
 
       </>,
         imageUrl: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3870&q=80",
-        codeSnippets: [
-          `function greet() {
-             console.log("Hello, world!");
-          }`,
-          `function add(a, b) {
-             return a + b;
-          }`
-        ],
     },
     {
         title: "Getting Started with Tailwind CSS",
